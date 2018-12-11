4fac0df7c09e20678ae1a352e5464e6bf5dd08bd
Victoria Ruebusch <owlsrsocutebro@gmail.com>
2018-12-11 07:36:53 -0700

    finished sketch?

--- final-project/sketch.js
+++ final-project/sketch.js
@@ -1,14 +1,16 @@
-
+let x = 100;
+let y = 350;
+let box_size = 600;
 var purrSound;
 
 function preload() {
 
    purrSound = loadSound("Purring.mp3");
+   meowSound = loadSound("meow.mp3");
 
     img = loadImage('Hand.png');
     img2 = loadImage('kitty.jpg');
-
-
+    img3 = loadImage('closed-eyes.jpg');
 }
 
 function setup() {
@@ -16,15 +18,26 @@ function setup() {
   createCanvas(600,windowHeight);
 
   purrSound.play();
-  purrSound.setVolume(1);
+  purrSound.setVolume(.7);
   purrSound.loop();
+}
+
+function mousePressed() {
+  meowSound.play();
 }
 
 function draw() {
 
-  noCursor();
-  image(img2, 0, 0, 600, windowHeight);
-  image(img, mouseX, mouseY, 450, 450);
+    noCursor();
 
+// kitty closes eyes when pet. open when not
+  if ((mouseX > x) && (mouseX < x + box_size) && (mouseY > y) && (mouseY < y +box_size)) {
+    image(img3, 0, 0, 600, windowHeight);
+  } else {
+    image(img2, 0, 0, 600, windowHeight);
+  }
+
+// HAND
+image(img, mouseX, mouseY, 300, 250);
 
 }
